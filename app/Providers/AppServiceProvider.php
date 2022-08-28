d28fbf8d5051312aab7488994214475a5d3672bb
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-08-28 22:29:29 +0100

    passport installation command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Console\KeysCommand;
+use Laravel\Passport\Console\ClientCommand;
+use Laravel\Passport\Console\InstallCommand;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +29,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+        Passport::routes();
+
+        $this->commands([
+            InstallCommand::class,
+            ClientCommand::class,
+            KeysCommand::class,
+        ]);
     }
 }
